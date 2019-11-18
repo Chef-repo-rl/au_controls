cb31181e3e6659bbe4658b6b9d61ee0c38a97f41
ashishraj91 <ashishraj65@gmail.com>
2019-11-18 14:46:00 -0800

    adding jenkinsfile

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,6 +15,6 @@ include_recipe 'au_controls::au_12_c_11'
 # include_recipe 'au_controls::au_12_c_9'
 # include_recipe 'au_controls::au_4_1'
 # include_recipe 'au_controls::au_5_b_1'
-include_recipe 'au_controls::au_8_1_a'
+# include_recipe 'au_controls::au_8_1_a'
 
 
